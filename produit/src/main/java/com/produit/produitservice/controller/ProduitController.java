805b6f661273cc7478fecb33ea75b41225ee5410
NGUEMALEU CEDRIC <houstonnguemaleu@gmail.com>
2025-02-07 20:19:20 +0100

    Ajout des endpoint au CRUD

--- produit/src/main/java/com/produit/produitservice/controller/ProduitController.java
+++ produit/src/main/java/com/produit/produitservice/controller/ProduitController.java
@@ -1,23 +1,21 @@
 package com.produit.produitservice.controller;
 
 import com.produit.produitservice.model.Produit;
+import com.produit.produitservice.repository.ProduitRepository;
 import com.produit.produitservice.service.ProduitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/produits")
 @RequiredArgsConstructor
 public class ProduitController {
     private final ProduitService produitService;
+    private final ProduitRepository produitRepository;
 
-    @GetMapping("/hello")
-    public String sayhello() {
-
-        return "Hello World";
-    }
 
     @GetMapping("/all")
     public List<Produit> getAllProduits(){
@@ -27,5 +25,20 @@ public class ProduitController {
     @PostMapping
     public Produit createProduit(@RequestBody Produit produit){
         return produitService.createProduit(produit);
+    }
+
+    @GetMapping("{id}")
+    public Produit getProduitById(@PathVariable long id){
+        return produitService.getProduitById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteProduitById(@PathVariable long id){
+        return produitService.deleteProduitById(id);
+    }
+
+    @PutMapping("{id}")
+    public Produit editProduitById(@PathVariable long id, @RequestBody Produit produit){
+        return produitService.editProduit(id, produit);
     }
 }
--- produit/src/main/java/com/produit/produitservice/service/ProduitService.java
+++ produit/src/main/java/com/produit/produitservice/service/ProduitService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +19,43 @@ public class ProduitService {
 
     public Produit createProduit(Produit produit) {
         return produitRepository.save(produit);
+    }
+
+    public Produit getProduitById(long id) {
+        Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+        if (optionalProduit.isEmpty()) {
+            throw new RuntimeException("Produit not found");
+        }
+        return optionalProduit.get();
+    }
+
+    public String deleteProduitById(long id) {
+        Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+        if (optionalProduit.isEmpty()) {
+            throw new RuntimeException("Suppression impossible produit not found");
+        }
+
+        produitRepository.delete(optionalProduit.get());
+
+        return "Produit deleted !";
+    }
+
+    public Produit editProduit(long id, Produit produit) {
+        Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+        if (optionalProduit.isEmpty()) {
+            throw new RuntimeException("Modification impossible produit not found");
+        }
+        Produit produitAModifier = optionalProduit.get();
+
+        produitAModifier.setName(produit.getName());
+        produitAModifier.setPrice(produit.getPrice());
+
+        return produitRepository.save(produitAModifier);
+
+
+
     }
 }
